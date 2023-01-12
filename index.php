f94e89d9a1d114b552582a8a335d9b906fbd17e9
Brett Scarlett <brett@scarletttech.com>
2023-01-12 00:45:35 -0500

    Initial commit + video series work

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>MyDemo</title>
+  <style>
+    body{
+      display:flex;
+      justify-content: center;
+      align-items:center;
+      height:100vh;
+      font-family: monospace;
+      margin:0;
+      
+    }
+    button{
+
+    }
+
+
+</style>
+</head>
+
+<html>
+    <body>
+
+      <h1>
+      
+        <?php
+
+        $myVar = "from new beginnings. ";
+        $thisVar = "the end. ";
+        $thisBool = false;
+        echo "This is " . (($thisBool == true) ? $myVar : $thisVar);  
+        
+        
+        ?>
+
+
+
+
+      </h1>
+      
+      <h5>
+        <?php
+
+
+
+        
+        ?>
+      </h5>
+
+    </body>
+</html> 
