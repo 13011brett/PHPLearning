c9466f3271f051639f9cb622b733245bd715255a
Brett Scarlett <brett@scarletttech.com>
2023-01-14 22:16:57 -0500

    Imported TailWinds / Separated Views

--- index.php
+++ index.php
@@ -1,33 +1,7 @@
-<!DOCTYPE html>
-<head>
-  <meta charset="utf-8">
-  <title>MyDemo</title>
-  <style>
-    body{
-      display:flex;
-      justify-content: center;
-      align-items:center;
-      height:100vh;
-      font-family: monospace;
-      margin:0;
-      
-    }
-    button{
 
-    }
-
-
-</style>
-</head>
-
-<html>
-    <body>
-
-      <h1>
       
         <?php
 
-use PgSql\Lob;
 
         $myVar = "from new beginnings. ";
         $thisVar = "the end. ";
@@ -36,39 +10,43 @@ use PgSql\Lob;
 
         $mySongs = ["Let It Go", "You're Gonna Go Far Kid", "Make A Man Out Of You"];
         $myBooks = [
-          [
-            'name' => 'Ulysses (1992)',
-            'author' => 'James Joyce',
-            'link' => 'https://en.wikipedia.org/wiki/Ulysses_(novel)'
-          ],
+            [
+                'name' => 'Ulysses (1992)',
+                'author' => 'James Joyce',
+                'link' => 'https://en.wikipedia.org/wiki/Ulysses_(novel)'
+            ],
+            [
+                'name' => 'test',
+                'author' => 'James Joyce',
+                'link' => 'https://en.wikipedia.org/wiki/Ulysses_(novel)'
+            ],
           [
             'name' => 'The Great Gatsby',
             'author' => 'F. Scott Fitzgerald',
-            'link' => 'http://en.wikipedia.org/wiki/The_Great_Gatsby'
+            'link' => 'https://en.wikipedia.org/wiki/The_Great_Gatsby'
           ]
 
 
         ];
         
-        
+        function filter($array, $fn )
+        {
+            $myArray = [];
+            foreach ($array as $book) {
+                if ($fn($book)) {
+                    $myArray[] = $book;
+                }
+
+            }
+            return $myArray;
+        }
+
+        $filteredBooks = filter($myBooks, function($book){
+            return ($book['name'] === 'test');
+        });
+
+        require "index.view.php";
         ?>
-
-
-
-
-      </h1>
       
     
 
-      <ul>
-        <?php
-
-        foreach ($myBooks as $book) : ?>
-        <li> <?= $book['name']; ?><ul> <?= $book['author']; ?> </li></ul> </li>
-        <?php endforeach ?>
-      </ul>
-      
-
-
-    </body>
-</html> 
