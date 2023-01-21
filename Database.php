04a69ccc7906e726dd2c2ef8b4811bed6ad38e78
Brett Scarlett <brett@scarletttech.com>
2023-01-21 18:21:05 -0500

    Config into a single file / refactor

--- Database.php
+++ Database.php
@@ -2,15 +2,21 @@
 
 class Database
 {
-
     public $connection;
 
-    public function __construct()
+
+    public function __construct($config, $username = 'brettowns', $password = '13011brett')
     {
 
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
 
-        $this->connection = new PDO($dsn, 'brettowns', '13011brett');
+        $dsn = 'mysql:' . (http_build_query($config, null,";" ));
+
+
+
+
+        $this->connection = new PDO($dsn, $username, $password,  [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query($myQuery)
--- index.php
+++ index.php
@@ -4,7 +4,10 @@ require 'functions.php';
 require 'Database.php';
 //require 'router.php';
 
-$db = new Database();
+
+$config = require('config.php');
+$db = new Database($config['database']);
+
 $posts = $db->query("select title from posts where id = 2")->fetchall(PDO::FETCH_ASSOC);
 
 
