ea3ebf5dcb333abf8a1432799551c3b58ec62ad9
Brett Scarlett <brett@scarletttech.com>
2023-01-25 14:49:24 -0500

    Further expanding upon notes view.

--- views/note.view.php
+++ views/note.view.php
@@ -7,10 +7,12 @@
 
         <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                <p class="mb-6">
+                    <a href="/notes" class="text-blue-500 underline bold">
+                        Go back...
+                    </a>
+                </p>
                 <p><?= $note['body'] ?></p>
-
-
-
 
             </div>
         </main>
