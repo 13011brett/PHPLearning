96a1cdc90803c20e42b14d104d836e7c63c2db10
Brett Scarlett <brett@scarletttech.com>
2023-01-27 20:43:16 -0500

    Minor semantic changes to error codes.

--- controllers/note.php
+++ controllers/note.php
@@ -9,17 +9,18 @@
 
         $note = $db->query("select * from notes where id = ?", [$_GET['id']])->fetch(PDO::FETCH_ASSOC);
 
+        $heading = "Note# {$note['id']}";
+        $currentUserId = 1;
+
         if(!$note){
 
             abort(404);
         }
-
-        if($note['user_id'] != 1){
-            abort(403);
+        if($note['user_id'] != $currentUserId){
+            abort(Response::FORIBDDEN);
         }
         //dd($notes);
 
-        $heading = "Note# {$note['id']}";
 
 
 
