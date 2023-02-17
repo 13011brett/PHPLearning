30fd3af7f7893b8c0b421bed7672c7140842434c
Brett Scarlett <brett@scarletttech.com>
2023-02-17 12:56:19 -0500

    Errors working properly now

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -8,26 +8,7 @@ $db = new Database($config['database']);
 require base_path('Core/Validator.php');
 
 $heading = 'Create Note';
-$maxChar = 1000;
-$errors = [];
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-
-    if(! Validator::textValidator($_POST['body'], 1, $maxChar)){
-        $errors['body'] = "A body of no more than {$maxChar} characters is required.";
-    }
-    if(! Validator::textValidator($_POST['subject'],1,30)){
-        $errors['subject'] = "A subject of no more than 30 characters is allowed";
-    }
-
-    if (empty($errors)) {
-        $db->query('INSERT INTO notes(body, user_id, subject) VALUES(:body, :id, :subject )', [
-            'body' => $_POST['body'],
-            'id' => 1,
-            'subject' => $_POST['subject']
-        ]);
-    }
-}
 
 view('notes/create.php', [
     'heading' => 'My Notes',
---
+++ controllers/notes/store.php
@@ -0,0 +1,38 @@
+<?php
+
+use Core\Database;
+use Core\Validator;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+require base_path('Core/Validator.php');
+
+$heading = 'Create Note';
+$maxChar = 1000;
+$errors = [];
+
+
+if (!Validator::textValidator($_POST['body'], 1, $maxChar)) {
+    $errors['body'] = "A body of no more than {$maxChar} characters is required.";
+}
+if (!Validator::textValidator($_POST['subject'], 1, 30)) {
+    $errors['subject'] = "A subject of no more than 30 characters is allowed";
+}
+if (!empty($errors)){
+    view('notes/create.php', [
+        'heading' => 'My Notes',
+        'errors' => $errors
+    ]);
+}
+
+   $db->query('INSERT INTO notes(body, user_id, subject) VALUES(:body, :id, :subject )', [
+        'body' => $_POST['body'],
+        'id' => 1,
+        'subject' => $_POST['subject']
+    ]);
+    header('location:/notes');
+
+
+
+
+
